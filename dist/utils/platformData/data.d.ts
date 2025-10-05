04da251df42d892a8366293654768398d9b0b252
SysPoe <panddplayz@outlook.com>
2025-10-05 14:32:18 +1000

    Add 'from' section, and ipswich/rosewood line

--- dist/utils/platformData/data.d.ts
+++ dist/utils/platformData/data.d.ts
@@ -2,8 +2,9 @@ export type Platform = {
     platform_code: number;
     trackName: string;
     trackCode: string;
+    from: string[];
     next: string[];
-    exitSide: "left" | "right" | "both";
+    exitSide: "left" | "right";
 };
 export type PlatformData = {
     [gtfs_stop_id: string]: Platform[];
