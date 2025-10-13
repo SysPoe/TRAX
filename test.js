16d29c7e0652ccc20f1361f7bd1af75fc52872d0
SysPoe <panddplayz@outlook.com>
2025-10-13 18:11:14 +1000

    move test.ts

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-import TRAX from "./index.js";
+import TRAX from "./dist/index.js";
 
 async function main() {
-	console.log("Loading GTFS data...");
+	console.log("Loading GTFS data...");	
 
 	let start_static = Date.now();
 	await TRAX.loadGTFS(false);
@@ -15,6 +15,7 @@ async function main() {
 
 	console.log(`GTFS loading took ${(end_static - start_static) / 1000} seconds.`);
 	console.log(`Realtime updates took ${(end_realtime - start_realtime) / 1000} seconds.`);
+	console.log("Done!");
 }
 
 main().catch(console.error);
