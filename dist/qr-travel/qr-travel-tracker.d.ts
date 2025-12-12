379b8b3eb19e1e09dcaa599845992a906ad53879
SysPoe <panddplayz@outlook.com>
2025-12-12 08:42:43 -0600

    Switch to ci/cd for building project

--- dist/qr-travel/qr-travel-tracker.d.ts
+++
@@ -1,7 +0,0 @@
-import type { GetServiceResponse, QRTPlace, ServiceLine, QRTService, ServiceUpdate, TravelTrip } from "./types.js";
-export declare function trackTrain(serviceID: string, serviceDate: string): Promise<GetServiceResponse>;
-export declare function getPlaces(): Promise<QRTPlace[]>;
-export declare function getServiceLines(): Promise<ServiceLine[]>;
-export declare function getAllServices(): Promise<QRTService[]>;
-export declare function getServiceUpdates(startDate?: string, endDate?: string): Promise<ServiceUpdate[]>;
-export declare function getCurrentQRTravelTrains(retries?: number): Promise<TravelTrip[]>;
--- dist/utils/stations.d.ts
+++
@@ -1,4 +0,0 @@
-import { AugmentedStop } from "./augmentedStop.js";
-import * as qdf from "qdf-gtfs";
-export declare function getGtfsStations(): qdf.Stop[];
-export declare function getStations(): AugmentedStop[];
